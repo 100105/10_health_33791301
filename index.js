2a23f95c2336116e764e1fa1b7419164fd04d6f6
Sara <sshah004@campus.goldsmiths.ac.uk>
2025-12-15 16:41:28 +0000

    index

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const expressSanitizer = require('express-sanitizer');
 const app = express();
 const port = 8000;
 
-/* views + static */
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(expressSanitizer());
 
-/* session */
+/*session*/
 app.use(
   session({
     secret: 'health-secret',
@@ -26,28 +26,36 @@ app.use(
   })
 );
 
-/* globals */
+/*variables*/
 app.use((req, res, next) => {
-  res.locals.isLoggedIn = !!req.session.userId;
-  res.locals.currentUser = req.session.userId || null;
-  next();
-});
+    res.locals.isLoggedIn = !!req.session.userId;
+    res.locals.currentUser = req.session.userId || null;
+    res.locals.basePath = '';
+    next();
+  });
+  
 
-/* database */
+/*db conenct*/
 const db = mysql.createPool({
   host: process.env.HEALTH_HOST,
   user: process.env.HEALTH_USER,
   password: process.env.HEALTH_PASSWORD,
   database: process.env.HEALTH_DATABASE
 });
+
 global.db = db;
 
-/* routes */
+/*main route*/
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+/*routes*/
 app.use('/', require('./routes/main'));
 app.use('/users', require('./routes/users'));
 app.use('/bookings', require('./routes/bookings'));
 
-/* server */
+
 app.listen(port, () => {
-  console.log('App running');
+  console.log(`App running on http://localhost:${port}`);
 });
